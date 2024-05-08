563a9e8997a185f6ef41da9b32235ea3e4533729
main <artem.graboveckii@gmail.com>
2024-05-08 20:10:08 +0300

    HW1

---
+++ app/index.php
@@ -0,0 +1,18 @@
+<?php
+$a = 'HELLO';
+$b = 'WORLD';
+$time = date('Y-m-d H:i:s');
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?= $a ?></title>
+</head>
+<body>
+<h1><?= $b ?></h1>
+<h2><?= $time ?></h2>
+</body>
+</html>
